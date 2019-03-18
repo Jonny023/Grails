76ecb4fc98cd7d58e6febb3dc2dacbe741683f4e
Jonny <342418262@qq.com>
2019-03-19 00:38:25 +0800

    update page.

--- grails-flowable/grails-app/controllers/grails/flowable/UrlMappings.groovy
+++ grails-flowable/grails-app/controllers/grails/flowable/UrlMappings.groovy
@@ -9,6 +9,14 @@ class UrlMappings {
             }
         }
 
+        get  "/$controller/$id(.$format)?/json"(action:"get")
+        put  "/$controller/$id(.$format)?/save"(action:"save")
+//        get  "/$controller/editor/getMenu"(action: "getMenu")
+        post "/$controller/create"(action: "newModel")
+        get  "/$controller/modelList"(action: "modelList")
+        get  "/$controller/deploy"(action: "deploy")
+        get  "/$controller/start"(action: "startProcess")
+
         "/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
